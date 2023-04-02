259e63811a8b5742d327e5f2a072b62dfc1e6738
crishpg <cristiano.crishpg@gmail.com>
2023-04-02 09:32:21 -0300

    add arquivos do projeto

---
+++ daemon.py
@@ -0,0 +1,16 @@
+import pymongo
+   
+   
+# establishing connection
+# to the database
+client = pymongo.MongoClient("mongodb://172.17.0.3:27017/")
+   
+# Database name
+db = client["meudb"]
+   
+# Collection name
+col = db["tmp"]
+ 
+# if we don't want to print id then pass _id:0
+for x in col.find({}, {"coursename": 1}):
+    print(x)
---
+++ dockerfile
@@ -0,0 +1,10 @@
+FROM python:3
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+EXPOSE 27017
+COPY . .
+COPY daemon.py /usr/src/app
+CMD [ "python", "./daemon.py" ]
